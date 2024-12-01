d0d3355105549377212d925611807dbfc1ece0e5
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2024-11-30 17:07:24 -0800

    feat(editor): enabled clangd support using -Y COMPILE_INFO

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# $HOME/dockerhome on host machine contains the docker specific config files
 
-docker run -it -v "$HOME/dockerhome":/home -v .:/work \
-  -e EDK2_DOCKER_USER_HOME=/home -w /work \
+docker run -it \
+  -v "$HOME/dockerhome":/home \
+  -v "$(realpath $PWD)":"$PWD" \
+  -e EDK2_DOCKER_USER_HOME=/home -w "$PWD" \
   fedora40-aarch64-edk2-dev:latest \
   /bin/bash edk2_build.sh
